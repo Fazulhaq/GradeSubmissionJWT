2c5b32d50956db677379f44afa98a11c80ee76c0
Fazulhaq Amin <128462337+Fazulhaq@users.noreply.github.com>
2023-09-05 11:43:05 +0430

    JWT Authorization Filter has been implemented into the security part.

--- src/main/java/com/ltp/gradesubmission/security/filter/ExceptionHandlerFilter.java
+++ src/main/java/com/ltp/gradesubmission/security/filter/ExceptionHandlerFilter.java
@@ -1,5 +1,6 @@
 package com.ltp.gradesubmission.security.filter;
 
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.ltp.gradesubmission.exception.EntityNotFoundException;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -13,9 +14,15 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try {
             filterChain.doFilter(request, response);
-        } catch (EntityNotFoundException e){
+        }catch (JWTVerificationException e){
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.getWriter().write("JWT is not valid");
+            response.getWriter().flush();
+        }
+        catch (EntityNotFoundException e){
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             response.getWriter().write("Username doest not exist");
+            response.getWriter().flush();
         } catch (RuntimeException e) {
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             response.getWriter().write("BAD REQUEST");
